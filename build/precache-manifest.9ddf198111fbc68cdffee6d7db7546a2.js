b591ea35b25de4027c3d79e1122deb4e17ed2c56
Roey Liba <rliba1990@gmail.com>
2020-05-20 15:45:10 +0300

    working list and game need more work

---
+++ build/precache-manifest.9ddf198111fbc68cdffee6d7db7546a2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "305f7dfa02d4be76a00137aa6daf5de2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "85b183bdea5704b3ae62",
+    "url": "/static/css/main.440886b8.chunk.css"
+  },
+  {
+    "revision": "165198ff7a2fe6da5881",
+    "url": "/static/js/2.c2100447.chunk.js"
+  },
+  {
+    "revision": "85b183bdea5704b3ae62",
+    "url": "/static/js/main.9a907e4c.chunk.js"
+  },
+  {
+    "revision": "a0318af035cae5114bf8",
+    "url": "/static/js/runtime-main.92690248.js"
+  }
+]);
